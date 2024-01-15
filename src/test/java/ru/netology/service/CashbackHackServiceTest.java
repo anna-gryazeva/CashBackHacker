cc233e35b632bc22f3a31e29f54214e6014583c8
Anna Gryazeva <anna_gryazeva@mail.ru>
2024-01-16 01:43:50 +0300

    Add tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,9 @@
 package ru.netology.service;
 
-import static org.testng.Assert.assertEquals;
+
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     @Test
@@ -14,4 +16,31 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
     }
 
+    @Test
+    public void boundaryValueCb1(){
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void boundaryValueCb2() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void boundaryValueCb3() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
 }
